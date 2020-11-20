8d93fe98faeebc20eebed5e6f1ffb6569d63d19d
karinamahi <karinahiratabueno@gmail.com>
2020-11-20 16:39:31 -0300

    config port env

--- main/server.js
+++ main/server.js
@@ -37,6 +37,8 @@ sockets.on('connection', (socket) => {
 
 game.start();
 
-server.listen(3000, ()=> {
+const port = process.env.PORT || '3000'
+
+server.listen(port, ()=> {
     console.log('Server running on port 3000.');
 })
